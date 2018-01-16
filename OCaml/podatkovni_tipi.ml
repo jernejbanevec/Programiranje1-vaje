92585223e90556dd74873aab366a8ce31d6152e9
Jernej Banevec <jernej.banevec@gmail.com>
2018-01-16 18:32:27 +0100

    Dokončal tipe.

--- OCaml/podatkovni_tipi.ml
+++ OCaml/podatkovni_tipi.ml
@@ -1,6 +1,19 @@
 (* Vojne čarodejov se nadaljujejo. *)
 
-(* brezvezn komit vaja *)
+(*let rec strongest_wizard (wizards : wizard list) : wizard option = 
+	match wizards with
+	| [] -> None
+	| [x] -> Some x
+	| x :: y :: tl -> 
+		let rec max_wizard (a, b) =
+			if a.ability > b.ability then a else b in
+		let new_list = max_wizard (x,y) :: tl in
+		strongest_wizard new_list*)
+
+(*let narejena_skoda spell (target : wizard) : int =
+	match effectiveness (school_of_spell spell) target.race with
+	| x -> int_of_float ((coef x) *. float_of_int (mana_of_spell spell)) *)
+	
 
 (* Čarodeji; ki se borijo v vojnah so pripadniki teh treh ras.  *)
 type race = Orc | Hobbit | Human
@@ -80,23 +93,31 @@ let snoop_dogg = {name = "Snoop Dogg"; ability = 420;  hp = 4000;  skills = [Bla
 
 
 (* Napiši funkcijo; ki iz seznama čarodejev vrne čarodeja z največ mane. *)
-let rec strongest_wizard (wizards : wizard list) : wizard option = 
+
+		
+let rec strongest_wizard (wizards : wizard list) : wizard option =
 	match wizards with
 	| [] -> None
-	| [x] -> Some x
-	| x :: y :: tl -> 
-		let rec max_wizard (a, b) =
-			if a.ability > b.ability then a else b in
-		let new_list = max_wizard (x,y) :: tl in
-		strongest_wizard new_list
-		
-
+	| [a] -> Some a
+	| hd1 :: hd2 :: tl -> if hd1.ability >= hd2.ability then strongest_wizard (hd1 :: tl)
+							else strongest_wizard (hd2 :: tl)
+							
+							
+	(* PRIMER BREZ TEGA KAM SLIKA JE SLABŠI, SAJ NAM SICER NE VRNE NAPAKE KO KODO FUNKCIJO NAROBE NAPIŠEM*)						
+(*let rec strongest_wizard wizards =
+	match wizards with
+	| [] -> None
+	| [a] -> Some a
+	| hd1 :: hd2 :: tl -> if hd1.ability >= hd2.ability then strongest_wizard (hd1 :: tl)
+							else strongest_wizard (hd2 :: tl)*)
+							
+							
 (* Posploši funkcijo strongest_wizard na funkcijo max_list; ki sprejme seznam
    in dodatno funkcijo dveh elementov max : 'a -> 'a -> 'a in vrne maksimalni element seznama
    glede na funkcijo max.
 *)
 
-let rec max_list (max_f : 'a -> 'a -> 'a)(l : 'a list): 'a option=
+let rec max_list (max_f : 'a -> 'a -> 'a)(l : 'a list): 'a option =
 	match l with
 	| [] -> None
 	| x :: [] -> Some x
@@ -105,6 +126,7 @@ let rec max_list (max_f : 'a -> 'a -> 'a)(l : 'a list): 'a option=
 		max_list max_f new_list (* TKO SI TI SPISOU *)
 
 		
+	(* URADNA REŠITEV - ENA OD NJIJU JE*)
 (*let rec max_list (max_f: 'a -> 'a -> 'a) (l: a' list) : 'a option =
 	match l with
 	| [] -> None
@@ -189,9 +211,8 @@ let coef = function
    int_of_float.
 *)
 
-let narejena_skoda spell (target : wizard) : int =
-	match effectiveness (school_of_spell spell) target.race with
-	| x -> int_of_float ((coef x) *. float_of_int (mana_of_spell spell))
+let narejena_skoda (spell : spell) (target : wizard) : int =
+	int_of_float((coef (effectiveness (school_of_spell spell) target.race)) *. float_of_int((mana_of_spell spell)))
 
 
 (* Zapiši funkcijo; ki vrne novo stanje čarodeja (z znižanimi življenskimi točkami [hp]);
@@ -205,14 +226,33 @@ let attack wizard spell = {wizard with hp = wizard.hp - (narejena_skoda spell wi
 
 (* Napiši funkcijo; ki za danega čarovnika izvršuje uroke; dokler ne izvede vseh urokov
    na seznamu; ali pa mu zmanjka točk mane. *)
-(*let cast_spells wizard = wizard * spell list = failwith "todo"*)
+   
+let cast_spells (caster : wizard) : wizard * spell list = 
+	let moc = caster.ability in
+		let sez = caster.skills in
+			let rec izmuci_casterja_aux moc sez acc =
+				match (moc, sez) with
+				| (0, _) -> ({caster with ability = 0}, List.rev acc)
+				| (a, []) -> ({caster with ability = a},List.rev acc)
+				| (m, hd :: tl) -> if m > (mana_of_spell hd) then izmuci_casterja_aux (moc - (mana_of_spell hd)) tl (hd :: acc) else ({caster with ability = m},List.rev acc)
+			in izmuci_casterja_aux moc sez []
+
 
 
 (* Napiši funkcijo; ki simulira spopad dveh čarodejev. V primeru; ko napadalec ne more izvršiti
    nobenega uroka; napadalec izgubi. V nasprotnem primeru uporabi vse uroke; ki jih lahko.
    Če branilcu zmanjka življenskih točk; izgubi; sicer pa se vlogi napadalca in branilca zamenjata.
 *)
-(*let rec duel (attacker : wizard) (defender : wizard) : wizard =
-  failwith "todo"*)
-
-(*let _ = duel frodo snoop_dogg*)
+let rec duel (attacker : wizard) (defender : wizard) : wizard =
+	let defender =
+		if attacker.hp > 0 then
+			let (attacker, napadi) = cast_spells defender in
+				match napadi with
+				| [] -> defender
+				| _ -> 
+					let defender = List.fold_left attack defender napadi in
+					duel defender attacker
+		else defender
+	in defender
+	
+let _ = duel frodo snoop_dogg
